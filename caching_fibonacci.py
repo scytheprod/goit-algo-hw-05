2229c837eb24c5e402be3255f33ecfa21398764b
scytheprod <vkrivtsov832@gmail.com>
2024-11-01 13:35:17 +0200

    Add files via upload

---
+++ caching_fibonacci.py
@@ -0,0 +1,23 @@
+def caching_fibonacci():
+    cache = {}
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 0:
+            return 1
+        elif n in (1,2):
+            return 1
+        elif n in cache:
+            return cache[n]
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    return fibonacci
+
+fib = caching_fibonacci()
+print(fib(15))
+
+
+
+
+
+
